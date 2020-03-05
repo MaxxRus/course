4550a8e2c588e0450a666e92e2187d7b701cddd4
Maxim Rusliachenko <rus.maxx@gmail.com>
2020-03-05 16:45:18 +0200

    start rest api go

--- start.go
+++ start.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-func main() {
-	fmt.Println("hello world")
+)
+
+type Article struct {
+	Title string `json:"Titlt"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request){
+articles := Articles{
+	Article{Title: "Test Title", Desc: "Test Description", Content: "Test Content"},
+}
+
+	fmt.Println(w, "Homepage Endpoint Hit")
+	json.NewEncoder(w).Encode(articles)
+}
+
+
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handlRequests(){
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main(){
+	handlRequests()
 }
